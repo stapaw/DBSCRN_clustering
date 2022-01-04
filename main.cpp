e0930cd3342b35fe5dc82f626815d2bdc041898e
stapaw <stanislawpawlak3@gmail.com>
2022-01-04 11:31:07 +0100

    add: stats file update, decreasing radius impl

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#import<cmath>
+#include<cmath>
 #include <queue>
 #include <fstream>
 #include <time.h>
 #include <json/json.h>
 #include <json/value.h>
 #include <json/writer.h>
+#include <functional>
 
 
 # include "csv.h"
 #include <boost/program_options.hpp>
+
 namespace po = boost::program_options;
 using namespace std;
 
@@ -31,6 +33,14 @@ struct distance_x {
     double dist{};
 };
 
+struct find_id : std::unary_function<distance_x, bool> {
+    int id;
+    find_id(int id):id(id) { }
+    bool operator()(distance_x const& m) const {
+        return m.id == id;
+    }
+};
+
 struct dist_comparator {
     inline bool operator()(const distance_x &struct1, const distance_x &struct2) {
         return (struct1.dist < struct2.dist);
@@ -38,13 +48,13 @@ struct dist_comparator {
 };
 
 
-void DBSCRN_expand_cluster(int i, int k, int i1);
+void DBSCRN_expand_cluster(int i, int k, int cluster_number);
 
 int get_cluster_of_nearest_core_point(int point, const vector<int> &vector);
 
 Json::Value
 prepare_stats_file(const string *clock_phases, const double *time_diffs, int number_of_phases, po::variables_map vm,
-                   int point_number, int dimensions);
+                   map<string, double> point_number);
 
 void write_to_out_file(int point_number);
 
@@ -52,7 +62,9 @@ void write_to_debug_file(int point_number);
 
 void
 write_to_stats_file(const string *clock_phases, const double *time_diffs, int number_of_phases, po::variables_map vm,
-                    int point_number, int dimensions);
+                    map<string, double> point_number);
+
+void calculate_knn_optimized(int k);
 
 vector<point> points;
 int clusters[100000] = {0};
@@ -78,10 +90,19 @@ vector<distance_x> calculate_distances_for_knn(point p, int distance_number) {
     return distances;
 }
 
+template<typename T>
+void print_queue(T q) { // NB: pass by value so the print uses a copy
+    while (!q.empty()) {
+        cout << q.top().id << ' ' << q.top().dist;
+        q.pop();
+    }
+    cout << '\n';
+}
 
 void calculate_knn(int k) {
     for (int i = 0; i < points.size(); i++) {
         vector<distance_x> distances = calculate_distances_for_knn(points.at(i), points.size());
+
         sort(distances.begin(), distances.end(), dist_comparator());
 //        cout << i << ":" << endl;
 //        for (distance_x j: distances) cout << j.id << ' ' << j.dist << endl;
@@ -100,14 +121,61 @@ void calculate_knn_optimized(point p, int k) {
     vector<distance_x> distances = calculate_distances_for_knn(r, points.size());
     sort(distances.begin(), distances.end(), dist_comparator());
     vector<distance_x> k_dist = calculate_distances_for_knn(p, k);
-    double radius = max_element(k_dist.begin(), k_dist.end(), dist_comparator())->dist;
-//    p_idx = find(distances.begin(), distances.end(), )
-//    while()
-    // calculate distances within border
-    // sort and return
+
+    priority_queue<distance_x, vector<distance_x>, dist_comparator> queue(k_dist.begin(), k_dist.end());
+    print_queue(queue);
+
+    double radius = queue.top().dist;
+    int p_idx = find_if(distances.begin(), distances.end(), find_id(p.id)) - distances.begin();
+//            (bind(&distance_x::id, placeholders::_1) == p.id));
+    cout<< "p_idx" << p_idx;
+    int up_idx = max(p_idx - 1, 0);
+    bool point_within_range_up = ((distances.at(up_idx).dist - distances.at(p_idx).dist) <= radius);
+    if(p.id != 0) {
+        while (point_within_range_up) {
+            double current_dist = calculate_distance(p, points.at(distances.at(up_idx).id));
+            if (current_dist <= radius) {
+                distance_x distance;
+                distance.id = distances.at(up_idx).id;
+                distance.dist = current_dist;
+                queue.push(distance);
+                queue.pop();
+                radius = queue.top().dist;
+            }
+            if (up_idx == 0)break;
+            up_idx = max(up_idx - 1, 0);
+            point_within_range_up = ((distances.at(up_idx).dist - distances.at(p_idx).dist) <= radius);
+        }
+    }
+    int max_index = distances.size() - 1;
+    int down_idx = min(p_idx + 1, max_index);
+    bool point_within_range_down = ((distances.at(down_idx).dist - distances.at(p_idx).dist) <= radius);
+    if(p.id != max_index) {
+        while (point_within_range_down) {
+            double current_dist = calculate_distance(p, points.at(distances.at(down_idx).id));
+            if (current_dist <= radius) {
+                distance_x distance;
+                distance.id = distances.at(down_idx).id;
+                distance.dist = current_dist;
+                queue.push(distance);
+                queue.pop();
+                radius = queue.top().dist;
+            }
+            if (down_idx == distances.size() - 1)break;
+            down_idx = min(down_idx + 1, max_index);
+            point_within_range_down = ((distances.at(down_idx).dist - distances.at(p_idx).dist) <= radius);
+        }
+    }
+    print_queue(queue);
+    for (int j = 0; j < k; j++) {
+        points.at(p.id).knn.push_back(queue.top().id);
+
+        points.at(queue.top().id).rnn.push_back(p.id);
+        queue.pop();
+    }
 }
 
-void DBSCRN(int k) {
+auto DBSCRN(int k) {
     vector<int> S_non_core;
     vector<int> S_core;
     int cluster_number = 1;
@@ -128,6 +196,12 @@ void DBSCRN(int k) {
         points.at(non_core_point).isCore = false;
         clusters[non_core_point] = get_cluster_of_nearest_core_point(non_core_point, S_core);
     }
+    struct result {
+        int cluster_number;
+        int core_points_number;
+        int non_core_points_number;
+    };
+    return result{cluster_number - 1, (int) S_core.size(), (int) S_non_core.size()};
 }
 
 int get_cluster_of_nearest_core_point(int point, const vector<int> &vector) {
@@ -162,7 +236,7 @@ void DBSCRN_expand_cluster(int i, int k, int cluster_number) {
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     clock_t start_time, input_read_time, sort_by_reference_point_time, rnn_neighbour_time, clustering_time, stats_calculation_time, output_write_time;
     const string clock_phases[] = {"read input file", "sort by reference point distances", "rnn calculation",
                                    "clustering", "stats calculation", "write output files", "total"};
@@ -170,13 +244,13 @@ int main(int argc, char* argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
             ("help", "produce help message")
-            ("in_file", po::value<string>()->default_value("../datasets/example.txt"), "input filename") // TODO: change to example
+            ("in_file", po::value<string>()->default_value("../datasets/example.txt"),
+             "input filename") // TODO: change to example
             ("alg", po::value<string>()->default_value("DBSCAN"), "algorithm name (DBSCAN|DBCSRN)")
             ("k", po::value<int>()->default_value(3), "number of nearest neighbors")
             ("eps", po::value<double>()->default_value(2), "eps parameter for DBSCAN")
             ("minPts", po::value<int>()->default_value(4), "minPts parameter for DBSCAN")
-            ("optimized", po::value<bool>()->default_value(false), "run optimized version")
-            ;
+            ("optimized", po::value<bool>()->default_value(false), "run optimized version");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -188,9 +262,9 @@ int main(int argc, char* argv[]) {
     }
 
     start_time = clock();
-    int k = vm["k"].as<int>();
-
     ifstream InputFile(vm["in_file"].as<string>());
+
+    int k = vm["k"].as<int>();
     int point_number, dimensions;
     InputFile >> point_number >> dimensions;
 //  TODO: update for more than 2 dimensions
@@ -210,8 +284,8 @@ int main(int argc, char* argv[]) {
 
     sort_by_reference_point_time = clock();
 
-    calculate_knn(k);
-
+//    calculate_knn(k);
+    calculate_knn_optimized(k);
     rnn_neighbour_time = clock();
 
 //    for(int i=0; i<point_number; i++){
@@ -227,7 +301,7 @@ int main(int argc, char* argv[]) {
 ////            cout << letters[j] << " ";
 //        }
 //    }
-    DBSCRN(k);
+    auto[cluster_number, core_points_number, non_core_points_number] = DBSCRN(k);
 
     clustering_time = clock();
     stats_calculation_time = clock();
@@ -248,15 +322,27 @@ int main(int argc, char* argv[]) {
     int number_of_phases = sizeof(clock_phases) / sizeof(*clock_phases);
     for (int i = 0; i < number_of_phases; i++)cout << clock_phases[i] << ": " << time_diffs[i] << endl;
 
-    write_to_stats_file(clock_phases, time_diffs, number_of_phases, vm, point_number, dimensions);
+    map<std::string, double> values{
+            {"#_of_points",           point_number},
+            {"#_of_point_dimensions", dimensions},
+            {"#_clusters",            cluster_number},
+            {"#_core_points",         core_points_number},
+            {"#_non_core_points",     non_core_points_number}};
+
+    write_to_stats_file(clock_phases, time_diffs, number_of_phases, vm, values);
+}
+
+void calculate_knn_optimized(int k) {
+    for (auto p : points) {
+        calculate_knn_optimized(p, k);
+    }
 }
 
 void
 write_to_stats_file(const string *clock_phases, const double *time_diffs, int number_of_phases, po::variables_map vm,
-                    int point_number, int dimensions) {
+                    map<string, double> values) {
     ofstream StatsFile("../stats_file");
-    Json::Value stats = prepare_stats_file(clock_phases, time_diffs, number_of_phases, std::move(vm), point_number,
-                                           dimensions);
+    Json::Value stats = prepare_stats_file(clock_phases, time_diffs, number_of_phases, std::move(vm), values);
     StatsFile << stats;
     StatsFile.close();
 }
@@ -268,7 +354,7 @@ void write_to_debug_file(int point_number) {
                   << "max_eps" << SEPARATOR
                   << "min_eps" << SEPARATOR // TODO: NDF ?
                   << "rnn_number";
-        for(int j: points.at(i).rnn){
+        for (int j: points.at(i).rnn) {
             DebugFile << SEPARATOR << j;
         }
         DebugFile << endl;
@@ -290,7 +376,7 @@ void write_to_out_file(int point_number) {
 
 Json::Value
 prepare_stats_file(const string *clock_phases, const double *time_diffs, int number_of_phases, po::variables_map vm,
-                   int point_number, int dimensions) {
+                   map<string, double> values) {
     Json::Value stats;
     Json::Value reference_point_values(Json::arrayValue); // TODO
     reference_point_values.append(Json::Value(4.2));
@@ -298,16 +384,19 @@ prepare_stats_file(const string *clock_phases, const double *time_diffs, int num
 
 
     stats["main"]["input_filename"] = vm["in_file"].as<string>();
-    stats["main"]["#_of_point_dimensions"] = dimensions;
-    stats["main"]["#_of_points"] = point_number;
+    stats["main"]["#_of_point_dimensions"] = values["#_of_point_dimensions"];
+    stats["main"]["#_of_points"] = values["#_of_points"];
+
     stats["parameters"]["algorithm"] = vm["alg"].as<string>();
     stats["parameters"]["k"] = vm["k"].as<int>();
     stats["parameters"]["Eps"] = vm["eps"].as<double>();
     stats["parameters"]["minPts"] = vm["minPts"].as<int>();
-    stats["main"]["#_clusters"] = 2; // TODO
+
+    stats["main"]["#_clusters"] = values["#_clusters"];
     stats["main"]["#_noise_points"] = 0; // TODO
-    stats["main"]["#_core_points"] = 0; // TODO
     stats["main"]["#_border_points"] = 0; // TODO
+    stats["main"]["#_core_points"] = values["#_core_points"];
+    stats["main"]["#non_core_points"] = values["#non_core_points"];
     stats["clustering_stats"]["avg_#_of_distance_calculation"] = 0; // TODO
 
     stats["clustering_stats"]["silhouette_coefficient"] = 0; // TODO
